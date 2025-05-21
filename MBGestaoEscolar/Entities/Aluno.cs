7ce6b4fdd666851adda78ddb17deb3aa7c51de1b
Cesar <cesar.cardoso@serpro.gov.br>
2025-05-21 17:55:41 -0300

    Add listagem de alunos

--- MBGestaoEscolar/Entities/Aluno.cs
+++ MBGestaoEscolar/Entities/Aluno.cs
@@ -1,25 +1,24 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MBGestaoEscolar.Entities
 {
+    [Table("aluno")]
     public class Aluno:Pessoa
     {
         public Aluno()
         {
         }
-        public Aluno(string matricula, DateTime? dataNascimento)
-        {
-            Matricula = matricula;
-            DataNascimento = dataNascimento;
-        }
-
+        
         public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
         public string Matricula { get; set; }
+        public string Email { get; set; }
+        public string? Telefone { get; set; }
         public DateTime? DataNascimento { get; set; }
-        public string Endereco { get; set; }
-        public string StatusAluno { get; set; }
-        public DateTime? DateTime {  get; set; }
-        public ICollection<Inscricao> Inscricoes { get; set; } = new List<Inscricao>();
+        public string? Endereco { get; set; }
+        public string StatusAluno { get; set; } = "Ativo";
+        public DateTime DataCadastro { get; set; }
 
     }
 }
